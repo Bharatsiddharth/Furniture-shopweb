17811b320ebb1768ce76702a00cbc0c15065cd0a
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-03-22 16:00:42 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
+
+
+var product = [
+    {names: "wooden", price:"10,000", image:"https://images.unsplash.com/photo-1549497538-303791108f95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"},
+    {names: "custon chair", price:"15,000", image:"https://images.unsplash.com/photo-1429087969512-1e85aab2683d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"},
+    {names: "whitechair", price:"12,000", image:"https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"},
+    {names: "wooden", price:"10,000", image:"https://images.unsplash.com/photo-1549497538-303791108f95?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"},
+    {names: "custon chair", price:"15,000", image:"https://images.unsplash.com/photo-1429087969512-1e85aab2683d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"},
+    {names: "whitechair", price:"12,000", image:"https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"},
+    
+    
+    
+
+]
+
+function addproduct(){
+    var clutter = '';
+
+product.forEach(function(product,idx){
+    clutter += `<div class="card1">
+    <img src="${product.image}" alt="">
+    <h3>${product.names}</h3>
+    <h4>${product.price}</h4>
+</div>`;
+
+})
+
+document.querySelector(".cards").innerHTML = clutter;
+}
+addproduct();
